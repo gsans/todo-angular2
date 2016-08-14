abff33624d5c5ce72105778d26d12e68396b7983
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2016-08-14 23:46:09 +0100

    Anonymise url

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 export const client = new ApolloClient({
-  networkInterface: createNetworkInterface('https://api.graph.cool/simple/v1/cirpiog0j09jw0156xp6me5wo')
+  networkInterface: createNetworkInterface('https://api.graph.cool/simple/v1/__APIURL__')
 });
