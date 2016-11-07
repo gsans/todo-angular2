5702aaa1824ecfc4bf44708060a11d3232407f82
Gerard Sans <gerard.sans@bigsystemstechnology.com>
2016-11-07 01:09:24 +0100

    Remove old version

--- src/client.ts
+++
@@ -1,5 +0,0 @@
-import ApolloClient, { createNetworkInterface } from 'apollo-client';
-
-export const client = new ApolloClient({
-  networkInterface: createNetworkInterface('https://api.graph.cool/simple/v1/__APIURL__')
-});
