2c49632136d58ce4e209f053fccecdc3b915aebc
Gerard Sans <gerard.sans@gmail.com>
2016-11-07 01:13:26 +0100

    Update client.ts
    Anonymise url

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,5 +1,5 @@
 import ApolloClient, { createNetworkInterface } from 'apollo-client';
 
 export const client = new ApolloClient({
-  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/cirpiog0j09jw0156xp6me5wo' })
+  networkInterface: createNetworkInterface({ uri: 'https://api.graph.cool/simple/v1/__APIURL__' })
 });
